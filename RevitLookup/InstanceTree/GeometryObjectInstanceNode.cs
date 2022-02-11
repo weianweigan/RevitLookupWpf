e1231b06f20a9018de297f5705a4b17b5a129789
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2022-02-11 14:35:45 +0700

    Merge info geometry detail with geometry instance by id

---
+++ RevitLookup/InstanceTree/GeometryObjectInstanceNode.cs
@@ -0,0 +1,15 @@
+﻿using Autodesk.Revit.DB;
+
+namespace RevitLookupWpf.InstanceTree
+{
+    public class GeometryObjectInstanceNode : InstanceNode<GeometryObject>
+    {
+        public GeometryObjectInstanceNode(GeometryObject rvtObjcet) : base(rvtObjcet)
+        {
+            if (rvtObjcet != null)
+            {
+                Name += $"({rvtObjcet.Id})";
+            }
+        }
+    }
+}

8ca0785f60644f36ab46deeb66d0e40394153073
weigan <1831197727@qq.com>
2022-08-13 23:19:23 +0800

    add nuget of helixtoolkit.wpf.sharpdx

---
+++ RevitLookup/View/PreviewWindow .xaml.cs
@@ -0,0 +1,31 @@
+﻿using RevitLookupWpf.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RevitLookupWpf.View
+{
+    /// <summary>
+    /// PreviewWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class PreviewWindow : Window
+    {
+        public PreviewWindow()
+        {
+            InitializeComponent();
+            DataContext = (ViewModel = new PreviewWindowViewModel());
+        }
+
+        public PreviewWindowViewModel ViewModel { get; }
+    }
+}
---
+++ RevitLookup/ViewModel/PreviewWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HelixToolkit.Wpf.SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitLookupWpf.ViewModel
+{
+    public class PreviewWindowViewModel : ObservableObject
+    {
+        public EffectsManager EffectsManager { get; }
+
+        public Camera Camera { get; }
+
+        public PreviewWindowViewModel()
+        {
+            EffectsManager = new DefaultEffectsManager();
+            Camera = new PerspectiveCamera();
+        }
+
+    }
+}
